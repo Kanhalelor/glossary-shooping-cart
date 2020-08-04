fac4aa4b8c125d6d4503f1441f8dac4ea703bf5d
robSyntax <48064476+Kanhalelor@users.noreply.github.com>
2020-08-04 15:42:38 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 
+const cross = document.getElementById('cancel').addEventListener('click', removeItem);
+
+function removeItem(e) {
+    let item = document.getElementById("cancel");
+    item.parentNode.style.display="none";
+}
+
+let p = [];
+// get all prices for all items
+const items = document.getElementsByClassName('price-row');
+for (let i = 0; i < items.length; i++) {
+    p.push(items[i].textContent);
+}
+
+
+const price = arr => p.map(Number);
+console.log(price);
+console.log(p)
