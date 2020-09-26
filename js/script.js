e7acb7a0ec175a280d4519699bc37d7978407124
robSyntax <48064476+Kanhalelor@users.noreply.github.com>
2020-09-26 12:59:45 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
-// select elements
+//=== select elements ====
 const numOfItems = document.querySelectorAll('input[type="number"]')
 const totalEl = document.getElementById('total-price')
 const itemPrice = document.querySelector('.price-cont')
+const container = document.querySelector('.card-container')
 
-// 
 
-let totalPrice = 0;
+// ======  functions =======
 
-// functions
 function selectElments(e) {
   let v = Number(e.target.value)
   console.log(v)
@@ -19,7 +18,6 @@ function selectElments(e) {
 }
 
 // update total price
-
 function updateTotalPrice(num, price) {
   let res = num * price;
   const prices = {
@@ -29,13 +27,22 @@ function updateTotalPrice(num, price) {
   totalEl.innerText = res;
 }
 
-// events
+//====== events =========
+
 (function() {
   numOfItems.forEach((item) => {
     item.addEventListener('input', selectElments);
   })
 })()
 
-// debug
+// remove element
+ container.addEventListener('click', (e) => {
+    if ( e.target.classList.contains('remove') ) {
+      let targetEl = e.target.parentElement.parentElement.parentElement.parentElement;
+      targetEl.style.display = 'none';
+    }
+  });
 
+//====== debug ===========
+// console.log(container)
 // console.log(total.textContent)
